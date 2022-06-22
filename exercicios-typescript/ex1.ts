84a761348d59017755f60a761d83853f6720e0a4
Vitor Shigueta <vshigueta@gmail.com>
2022-06-22 06:07:56 -0300

    Primeiro commit

---
+++ exercicios-typescript/ex1.ts
@@ -0,0 +1,15 @@
+/* Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+const employee = {
+    codigo: 10,
+    nome: 'João'
+};
+
+console.log(employee)
+
+export{}
